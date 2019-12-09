58fde1518146f565a6edddd70f2c618057f1af54
ferdiansahgg <ferdiayana@gmail.com>
2019-12-09 14:52:00 +0700

    Second Commit

--- models/models.py
+++ models/models.py
@@ -10,9 +10,16 @@ class vit_report_balance(models.Model):
     date_start = fields.Date()
     date_end = fields.Date()
     company_id = fields.Many2one(comodel_name="res.company")
+    report_details_ids = fields.One2many(
+        comodel_name="vit_report_balance_so", inverse_name="report_id"
+    )
 
 
 class report_balance_so(models.Model):
     _name = "vit_report_balance_so"
 
     report_id = fields.Many2one(comodel_name="vit_report_balance.vit_report_balance")
+    product_id = fields.Many2one(comodel_name="product.product")
+    product_code = fields.Char()
+    total_so_bln_lalu = fields.Float()
+    total_so_bln_ini = fields.Float()
